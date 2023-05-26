9f10745379f64e82436790b731a6a60a73e9f8a8
Paulo1402 <paulobenatto02@gmail.com>
2023-05-26 14:52:58 -0300

    added css and inicial structure

--- contact/admin.py
+++ contact/admin.py
@@ -4,12 +4,7 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "first_name",
-        "last_name",
-        "phone",
-    )
+    list_display = ("id", "first_name", "last_name", "phone", "show")
     ordering = ("-id",)
     # list_filter = 'created_date',
     search_fields = (
@@ -19,10 +14,7 @@ class ContactAdmin(admin.ModelAdmin):
     )
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = (
-        "first_name",
-        "last_name",
-    )
+    list_editable = ("first_name", "last_name", "show")
 
 
 @admin.register(models.Category)
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from . import models
+from contact import models
 
 
 def index(request):
-    contacts = models.Contact.objects.all()  # pylint: disable=E1101
+    contacts = models.Contact.objects.filter(show=True).order_by('-id')  # pylint: disable=E1101
 
     return render(request, "contact/index.html", {'contacts': contacts})
